ceb194611e437dfb5eec7822d88aa68642a29f87
Bhavesh Gupta <BhaveshSGupta@users.noreply.github.com>
2017-01-24 23:03:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ var MsTranslator = require('mstranslator');
 //   keyFilename: './FlockGoogleTranslate-c6672cb57344.json'
 // });
 var app = express();
-var client = new MsTranslator({
-  api_key: "431e427ba582406e87ba31290c7bb8ad" // use this for the new token API. 
-}, true);
+
 flock.setAppId(config.appId);
 flock.setAppSecret(config.appSecret);
 
@@ -49,12 +47,15 @@ process.on('exit', function() {
     fs.writeFileSync('./tokens.json', JSON.stringify(tokens));
 });
 flock.events.on('client.slashCommand', function(event) {
+  var trans = new MsTranslator({
+  api_key: "431e427ba582406e87ba31290c7bb8ad" // use this for the new token API. 
+}, true);
      var params = {
   text: event.text
   , from: 'en'
   , to: 'es'
 };
-client.translate(params, function(err, data) {
+trans.translate(params, function(err, data) {
     flock.callMethod('chat.sendMessage', getUserToken(event.userId), {
         to: event.chat,
         text: data
@@ -72,77 +73,3 @@ client.translate(params, function(err, data) {
 getUserToken = function(userId) {
     return tokens[userId];
 }
-// // googleTranslate.translate('My name is Brandon', 'es', function(err, translation) {
-// //   console.log(translation.translatedText);
-// //   // =>  Mi nombre es Brandon
-// });
-// // Imports the Google Cloud client library
-
-
-// // Your Google Cloud Platform project ID
-// const projectId = 'YOUR_PROJECT_ID';
-
-// // Instantiates a client
-// const translateClient = Translate({
-//     projectId: projectId
-// });
-
-// // The text to translate
-// const text = 'Hello, world!';
-// // The target language
-// const target = 'ru';
-
-// // Translates some text into Russian
-// translateClient.translate(text, target)
-//     .then((results) => {
-//         const translation = results[0];
-
-//         console.log(`Text: ${text}`);
-//         console.log(`Translation: ${translation}`);
-//     });
-
-// function translateText(input, target) {
-//   // The text to translate, e.g.:
-//   // input = 'Hello, world';
-//   // The target language, e.g.:
-//   // target = 'ru';
-
-//   if (!Array.isArray(input)) {
-//     input = [input];
-//   }
-
-//   // Instantiates a client
-//   const translate = Translate();
-
-//   // Translates the text into the target language. "input" can be a string for
-//   // translating a single piece of text, or an array of strings for translating
-//   // multiple texts.
-//   return translate.translate(input, target)
-//     .then((results) => {
-//       let translations = results[0];
-//       translations = Array.isArray(translations) ? translations : [translations];
-
-//       console.log('Translations:');
-//       translations.forEach((translation, i) => {
-//         console.log(`${input[i]} => (${target}) ${translation}`);
-//       });
-
-//       return translations;
-//     });
-// }
-
-// Second parameter to constructor (true) indicates that
-// the token should be auto-generated.
-
-// // old token API
-// var client = new MsTranslator({
-//   client_id: "your client_id" // use this for the old token API
-//   , client_secret: "your client secret" // use this for the old token API
-// }, true);
-
-// new token API
-
-
-
-
-// Don't worry about access token, it will be auto-generated if needed.
