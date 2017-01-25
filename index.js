8de1d620d7452bb5fa398ad4c85771cfc5a317ca
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-01-25 16:08:41 +0530

    update1

--- index.js
+++ index.js
@@ -89,3 +89,25 @@ getUserToken = function(userId) {
 getUserTokenLan = function(userId) {
     return lang[userId];
 }
+app.get('/events', function (req, res) {
+    console.log('request query: ', req.query);
+    var userId = res.locals.eventTokenPayload.userId;
+    console.log('user id: ', userId);
+    // var event = JSON.parse(req.query.flockEvent);
+    // if (event.userId !== userId) {
+    //     console.log('userId in event doesn\'t match the one in event token');
+    //     res.sendStatus(403);
+    //     return;
+    // }
+    // console.log('event: ', event);
+    // res.set('Content-Type', 'text/html');
+    // var list = store.listScraps(userId, event.chat);
+    // console.log('list: ', list);
+    // if (list) {
+    //     list = list.map(function (text) {
+    //         return text.replace(urlRegex, '<a href="$&">$&</a>');
+    //     });
+    // }
+    // var body = Mustache.render(widgetTemplate, { list: list, event: event });
+    res.send("hello");
+});
